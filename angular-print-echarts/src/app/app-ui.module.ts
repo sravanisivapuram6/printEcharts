78c43cb9695ba9c48933e7defc6897d455cb602f
sravanisivapuram6 <sravani.s@hng.co.in>
2022-06-27 18:09:26 +0530

    added angular material.

---
+++ angular-print-echarts/src/app/app-ui.module.ts
@@ -0,0 +1,125 @@
+import { MatButtonToggleModule } from "@angular/material/button-toggle";
+import { MatCheckboxModule } from "@angular/material/checkbox";
+import { MatChipsModule } from "@angular/material/chips";
+import { MatDatepickerModule } from "@angular/material/datepicker";
+import { MatDialogModule } from "@angular/material/dialog";
+import { MatDividerModule } from "@angular/material/divider";
+import { MatExpansionModule } from "@angular/material/expansion";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatGridListModule } from "@angular/material/grid-list";
+import { MatIconModule } from "@angular/material/icon";
+import { MatInputModule } from "@angular/material/input";
+import { MatMenuModule } from "@angular/material/menu";
+import { MatSelectModule } from "@angular/material/select";
+import { MatSnackBarModule } from "@angular/material/snack-bar";
+import { MatTableModule } from "@angular/material/table";
+import { MatTabsModule } from "@angular/material/tabs";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
+import { MatSlideToggleModule } from "@angular/material/slide-toggle";
+import { MatRadioModule } from "@angular/material/radio";
+import { NgModule } from "@angular/core";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+// import { MatMomentDateModule } from "@angular/material-moment-adapter";
+import { FlexLayoutModule } from "@angular/flex-layout";
+import { MatBadgeModule } from "@angular/material/badge";
+import { MatButtonModule } from "@angular/material/button";
+import { MatCardModule } from "@angular/material/card";
+import { MatListModule } from "@angular/material/list";
+import { MatProgressBarModule } from "@angular/material/progress-bar";
+import { MatTooltipModule } from "@angular/material/tooltip";
+import { MatPaginatorModule } from "@angular/material/paginator";
+import {MatNativeDateModule, MatRippleModule} from "@angular/material/core";
+import { MatAutocompleteModule } from "@angular/material/autocomplete";
+import { MatSliderModule } from "@angular/material/slider";
+import { ClipboardModule } from '@angular/cdk/clipboard';
+import { MatSortModule } from "@angular/material/sort";
+import { MatStepperModule } from "@angular/material/stepper";
+import {MatSidenavModule} from '@angular/material/sidenav';
+
+@NgModule({
+  imports: [
+    MatButtonModule,
+    MatCheckboxModule,
+    MatDialogModule,
+    MatIconModule,
+    FlexLayoutModule,
+    MatDividerModule,
+    MatTabsModule,
+    MatDatepickerModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    MatToolbarModule,
+    MatTableModule,
+    MatChipsModule,
+    MatExpansionModule,
+    MatSnackBarModule,
+    MatSelectModule,
+    MatMenuModule,
+    MatGridListModule,
+    MatCardModule,
+    // MatMomentDateModule,
+    MatListModule,
+    MatButtonToggleModule,
+    MatStepperModule,
+    MatProgressBarModule,
+    MatTooltipModule,
+    MatGridListModule,
+    MatProgressSpinnerModule,
+    MatSlideToggleModule,
+    MatRadioModule,
+    MatPaginatorModule,
+    MatRippleModule,
+    MatAutocompleteModule,
+    MatBadgeModule,
+    MatSliderModule,
+    ClipboardModule,
+    MatSortModule,
+    MatNativeDateModule,
+    MatSidenavModule,
+  ],
+  exports: [
+    MatButtonModule,
+    MatCheckboxModule,
+    MatDialogModule,
+    MatIconModule,
+    FlexLayoutModule,
+    MatDividerModule,
+    MatTabsModule,
+    MatDatepickerModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    MatToolbarModule,
+    MatTableModule,
+    MatChipsModule,
+    MatExpansionModule,
+    MatSnackBarModule,
+    MatSelectModule,
+    MatMenuModule,
+    MatGridListModule,
+    MatCardModule,
+    // MatMomentDateModule,
+    MatListModule,
+    MatButtonToggleModule,
+    MatStepperModule,
+    MatProgressBarModule,
+    MatTooltipModule,
+    MatGridListModule,
+    MatProgressSpinnerModule,
+    MatSlideToggleModule,
+    MatRadioModule,
+    MatPaginatorModule,
+    MatRippleModule,
+    MatAutocompleteModule,
+    MatBadgeModule,
+    MatSliderModule,
+    ClipboardModule,
+    MatSortModule,
+    MatSidenavModule
+  ]
+})
+export class MedexMaterialModule { }
